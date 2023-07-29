0ecbe74ace1569f7ce976f325ebd8d3d6cfd9279
Sait Ketshongo <ketshongprofessional@gmail.com>
2023-07-29 11:43:37 -0700

    last part mandentory

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description : main -prints out a string to stdout.
+ * Return : 0 if success
+ */
+int main(void)
+{
+puts("\"Programming is like buiding a multilingual puzzle");
+return (0);
+}
